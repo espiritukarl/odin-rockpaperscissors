e3a4aa65a2bb22dbc5b9ebe893a95f90cec1e4a7
Karl Espiritu <karlespiritukarl@gmail.com>
2022-02-13 23:36:34 +0800

    Updated it, cleaned up codes and fixed a bit of CSS

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 let computerWins = 0;
 let playerWins = 0;
-let playerChoice;
 
 const buttonPressed = document.querySelectorAll(".player-button");
-const playerRock = document.getElementsByClassName("player-button rock");
-const playerPaper = document.getElementsByClassName("player-button paper");
-const playerScissors = document.getElementsByClassName("player-button scissors");
 
 const computerButton = document.querySelectorAll(".computer-button");
-const computerRock = document.querySelector(".crock");
-const computerScissors = document.querySelector(".cscissors");
-const computerPaper = document.querySelector(".cpaper");
+const computerRock = document.querySelector(".rock");
+const computerScissors = document.querySelector(".scissors");
+const computerPaper = document.querySelector(".paper");
 
 const playerResults = document.querySelector("#player-results");
 const computerResults = document.querySelector("#computer-results");
@@ -39,14 +35,45 @@ let rockPaperScissors = (choice) => {
 let playRound = (playerChoice, computerSelection) => {
     computerChoice = rockPaperScissors(computerSelection);
     if (playerChoice == computerChoice) {
-        return 0; //draw
+        return "draw";
     } else if ((playerChoice == 'rock' && computerChoice == 'paper') ||
         (playerChoice == 'paper' && computerChoice == 'scissors') ||
         (playerChoice == 'scissors' && computerChoice == 'rock')) {
-        return 1; //lost
+        return "lost";
     }
     else {
-        return 2; //win
+        return "win";
+    }
+};
+
+let roundResults = (playerChoice, computerSelection) => {
+
+    if (playRound(playerChoice, computerSelection) == "draw") {
+        playerDialogue.textContent = `Draw! You both chose ${playerChoice}`;
+        computerDialogue.textContent = `Draw! You both chose ${playerChoice}`;
+    } else if (playRound(playerChoice, computerSelection) == "lost") {
+        computerWins++;
+        playerDialogue.textContent = `You lost! Your oppenent chose ${rockPaperScissors(computerSelection)}!`;
+        computerDialogue.textContent = `Computer win! His ${rockPaperScissors(computerSelection)} beat your ${playerChoice}.`;
+    } else if (playRound(playerChoice, computerSelection) == "win") {
+        playerWins++;
+        computerDialogue.textContent = `Computer lost! His ${rockPaperScissors(computerSelection)} lost to your ${playerChoice}.`;
+        playerDialogue.textContent = `Player Win! Your ${playerChoice} beat the computer's ${rockPaperScissors(computerSelection)}!`;
+    } else {
+        console.log("An Error occured!");
+    }
+
+    if (computerWins == 5) {
+        computerWins = 0;
+        playerWins = 0;
+        computerResults.textContent = "Congrats to the Computer as he is the first to get to 5 wins!";
+    } else if (playerWins == 5) {
+        computerWins = 0;
+        playerWins = 0;
+        playerResults.textContent = "Congrats to the Player as he is the first to get to 5 wins!";
+    } else {
+        playerResults.textContent = `Wins: ${playerWins}`;
+        computerResults.textContent = `Wins: ${computerWins}`;
     }
 };
 
@@ -57,61 +84,15 @@ computerButton.forEach((button) => {
     });
 });
 
-
 buttonPressed.forEach((button) => {
     button.addEventListener('click', e => {
         let computerSelection = Math.floor(Math.random() * 3);
-        computerRock.classList.remove("hovered");
-        computerPaper.classList.remove("hovered");
-        computerScissors.classList.remove("hovered");
 
-        if (playerWins == 0 || computerWins == 0) {
-            playerResults.textContent = "Wins: 0";
-            computerResults.textContent = "Wins: 0";
-        }
+        computerButton.forEach((button) => {
+            button.classList.remove("hovered");
+        });
 
-
-        if (e.path[0] == playerRock[0]) {
-            playerChoice = "rock";
-        } else if (e.path[0] == playerPaper[0]) {
-            playerChoice = "paper";
-        } else if (e.path[0] == playerScissors[0]) {
-            playerChoice = "scissors";
-        }
-
-        if (playRound(playerChoice, computerSelection) == 0) {
-            playerDialogue.textContent = `Draw! You both chose ${playerChoice}`;
-            computerDialogue.textContent = `Draw! You both chose ${playerChoice}`;
-        } else if (playRound(playerChoice, computerSelection) == 1) {
-            computerWins++;
-            playerDialogue.textContent = `You lost! Your oppenent chose ${rockPaperScissors(computerSelection)}!`;
-            computerDialogue.textContent = `Computer win! His ${rockPaperScissors(computerSelection)} beat your ${playerChoice}.`;
-            computerResults.textContent = `Wins: ${computerWins}`;
-        } else if (playRound(playerChoice, computerSelection) == 2) {
-            playerWins++;
-            computerDialogue.textContent = `Computer lost! His ${rockPaperScissors(computerSelection)} lost to your ${playerChoice}.`;
-            playerDialogue.textContent = `Player Win! Your ${playerChoice} beat the computer's ${rockPaperScissors(computerSelection)}!`;
-            playerResults.textContent = `Wins: ${playerWins}`;
-        } else {
-            console.log("An Error occured!");
-        }
-
-        if (computerWins == 5) {
-            computerWins = 0;
-            playerWins = 0;
-
-            computerResults.textContent = "Congrats to the Computer as he is the first to get to 5 wins!";
-            computerContainer.appendChild(computerResults);
-
-            playerResults.textContent = `Wins: ${playerWins}`;
-            playerContainer.appendChild(playerWins)
-
-        } else if (playerWins == 5) {
-            computerWins = 0;
-            playerWins = 0;
-
-            playerResults.textContent = "Congrats to the Player as he is the first to get to 5 wins!";
-            computerResults.textContent = `Wins: ${computerWins}`;
-        }
+        let playerChoice = (e.target.textContent).toLowerCase();
+        roundResults(playerChoice, computerSelection);
     });
 });
